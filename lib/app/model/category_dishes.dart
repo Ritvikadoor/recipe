e72582259bfaabb105748c31ab9ae18e64cd40a1
Ritvik_Pramod <103210186+Ritvikadoor@users.noreply.github.com>
2022-10-24 16:24:26 +0530

    model class created

---
+++ lib/app/model/category_dishes.dart
@@ -0,0 +1,59 @@
+import 'package:reciepe_app/app/model/models.dart';
+
+class CategoryDish {
+  CategoryDish({
+    required this.dishId,
+    required this.dishName,
+    required this.dishPrice,
+    required this.dishImage,
+    required this.dishCurrency,
+    required this.dishCalories,
+    required this.dishDescription,
+    required this.dishAvailability,
+    required this.dishType,
+    required this.nexturl,
+    required this.addonCat,
+  });
+
+  String dishId;
+  String dishName;
+  double dishPrice;
+  String dishImage;
+  String dishCurrency;
+  int dishCalories;
+  String dishDescription;
+  bool dishAvailability;
+  int dishType;
+  String nexturl;
+  List<AddonCat> addonCat;
+
+  factory CategoryDish.fromJson(Map<String, dynamic> json) => CategoryDish(
+        dishId: json["dish_id"],
+        dishName: json["dish_name"],
+        dishPrice: json["dish_price"].toDouble(),
+        dishImage: json["dish_image"],
+        dishCurrency: dishCurrencyValues.map[json["dish_currency"]].toString(),
+        dishCalories: json["dish_calories"],
+        dishDescription: json["dish_description"],
+        dishAvailability: json["dish_Availability"],
+        dishType: json["dish_Type"],
+        nexturl: json["nexturl"] == null ? null : json["nexturl"],
+        addonCat: [],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "dish_id": dishId,
+        "dish_name": dishName,
+        "dish_price": dishPrice,
+        "dish_image": dishImage,
+        "dish_currency": dishCurrencyValues.reverse[dishCurrency],
+        "dish_calories": dishCalories,
+        "dish_description": dishDescription,
+        "dish_Availability": dishAvailability,
+        "dish_Type": dishType,
+        "nexturl": nexturl == null ? null : nexturl,
+        "addonCat": addonCat == null
+            ? null
+            : List<dynamic>.from(addonCat.map((x) => x.toJson())),
+      };
+}
--- lib/app/model/models.dart
+++ lib/app/model/models.dart
@@ -1,60 +1,10 @@
 // To parse this JSON data, do
 //
-//     final welcome = welcomeFromJson(jsonString);
+//     final HotelModel = HotelModelFromJson(jsonString);
 
 import 'dart:convert';
 
-List<Welcome> welcomeFromJson(String str) =>
-    List<Welcome>.from(json.decode(str).map((x) => Welcome.fromJson(x)));
-
-String welcomeToJson(List<Welcome> data) =>
-    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
-
-class Welcome {
-  Welcome({
-    required this.restaurantId,
-    required this.restaurantName,
-    required this.restaurantImage,
-    required this.tableId,
-    required this.tableName,
-    required this.branchName,
-    required this.nexturl,
-    required this.tableMenuList,
-  });
-
-  String restaurantId;
-  String restaurantName;
-  String restaurantImage;
-  String tableId;
-  String tableName;
-  String branchName;
-  String nexturl;
-  List<TableMenuList> tableMenuList;
-
-  factory Welcome.fromJson(Map<String, dynamic> json) => Welcome(
-        restaurantId: json["restaurant_id"],
-        restaurantName: json["restaurant_name"],
-        restaurantImage: json["restaurant_image"],
-        tableId: json["table_id"],
-        tableName: json["table_name"],
-        branchName: json["branch_name"],
-        nexturl: json["nexturl"],
-        tableMenuList: List<TableMenuList>.from(
-            json["table_menu_list"].map((x) => TableMenuList.fromJson(x))),
-      );
-
-  Map<String, dynamic> toJson() => {
-        "restaurant_id": restaurantId,
-        "restaurant_name": restaurantName,
-        "restaurant_image": restaurantImage,
-        "table_id": tableId,
-        "table_name": tableName,
-        "branch_name": branchName,
-        "nexturl": nexturl,
-        "table_menu_list":
-            List<dynamic>.from(tableMenuList.map((x) => x.toJson())),
-      };
-}
+import 'package:reciepe_app/app/model/category_dishes.dart';
 
 class TableMenuList {
   TableMenuList({
@@ -123,64 +73,6 @@ class AddonCat {
       };
 }
 
-class CategoryDish {
-  CategoryDish({
-    required this.dishId,
-    required this.dishName,
-    required this.dishPrice,
-    required this.dishImage,
-    required this.dishCurrency,
-    required this.dishCalories,
-    required this.dishDescription,
-    required this.dishAvailability,
-    required this.dishType,
-    required this.nexturl,
-    required this.addonCat,
-  });
-
-  String dishId;
-  String dishName;
-  double dishPrice;
-  String dishImage;
-  String dishCurrency;
-  int dishCalories;
-  String dishDescription;
-  bool dishAvailability;
-  int dishType;
-  String nexturl;
-  List<AddonCat> addonCat;
-
-  factory CategoryDish.fromJson(Map<String, dynamic> json) => CategoryDish(
-        dishId: json["dish_id"],
-        dishName: json["dish_name"],
-        dishPrice: json["dish_price"].toDouble(),
-        dishImage: json["dish_image"],
-        dishCurrency: dishCurrencyValues.map[json["dish_currency"]].toString(),
-        dishCalories: json["dish_calories"],
-        dishDescription: json["dish_description"],
-        dishAvailability: json["dish_Availability"],
-        dishType: json["dish_Type"],
-        nexturl: json["nexturl"] == null ? null : json["nexturl"],
-        addonCat: [],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "dish_id": dishId,
-        "dish_name": dishName,
-        "dish_price": dishPrice,
-        "dish_image": dishImage,
-        "dish_currency": dishCurrencyValues.reverse[dishCurrency],
-        "dish_calories": dishCalories,
-        "dish_description": dishDescription,
-        "dish_Availability": dishAvailability,
-        "dish_Type": dishType,
-        "nexturl": nexturl == null ? null : nexturl,
-        "addonCat": addonCat == null
-            ? null
-            : List<dynamic>.from(addonCat.map((x) => x.toJson())),
-      };
-}
-
 enum DishCurrency { SAR }
 
 final dishCurrencyValues = EnumValues({
@@ -197,7 +89,7 @@ class EnumValues<T> {
 
   Map<T, String> get reverse {
     if (reverseMap == null) {
-      reverseMap = map.map((k, v) => new MapEntry(v, k));
+      reverseMap = map.map((k, v) => MapEntry(v, k));
     }
     return reverseMap;
   }
